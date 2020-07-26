5d33cc7b47be7e3cbfc8054b0799c2eacf7ae58d
Brian <brian_liang95827@163.com>
2020-07-26 23:20:57 +0800

    second

--- week04/toy-browser/parser.js
+++ week04/toy-browser/parser.js
@@ -1,11 +1,25 @@
+let currentToken = null;
+
+function emit(token) {
+	console.log(token);
+}
+
+
 const EOF = Symbol("EOF");
-/*
+
 function data(c){
   if(c == '<') {
    	 	return tagOpen;
   } else if(c == EOF) {
+		emit({
+			type:"EOF"
+		});
 		return ;
   } else {
+	  emit({
+		  type:"text",
+		  content:c
+	  })
 	    return data;
   }
 } 
@@ -14,6 +28,10 @@ function tagOpen(c){
   if(c == '/') {
     	return endTagOpen;
   } else if(c.match(/^[a-zA-Z]$/)) {
+	  	currentToken = {
+			  type:"startTag",
+			  tagName : ""
+		  }
     	return tagName(c);
   } else {
     	return ;
@@ -22,6 +40,10 @@ function tagOpen(c){
 
 function endTagOpen(c){
   if(c.match(/^[a-zA-Z]$/)) {
+	  	currentToken = {
+			  type: "endTag",
+			  tagName: ""
+		  }
     	return tagName(c);
   } else if(c == '>') {
 
@@ -38,8 +60,10 @@ function tagName(c){
 	} else if(c == '/') {
 		  return selfClosingStartTag;
 	  } else if(c.match(/^[a-zA-Z]$/)) {
+		  	currentToken.tagName += c;
 			return tagName;
 	  } else if(c == '>') {
+		  emit(currentToken);
 		  return data;
 	  } else {
 		  return tagName;
@@ -49,13 +73,115 @@ function tagName(c){
 function beforeAttributeName(c){
 	if(c.match(/^[\t\n\f ]$/)) {
 		return beforeAttributeName;
-	} else if(c == '>') {
-		return data;
+	} else if(c == '/' || c == '>' || c == 'EOF') {
+		return afterAttributeName(c);
 	} else if(c == '=') {
-		return beforeAttributeName;
+		
 	} else {
-		return beforeAttributeName;
+		currentAttribute = {
+			name:"",
+			value:""
+		}
+		return attributeName(c);
 	}
+}
+
+function attributeName(c){
+	if(c.match(/^[\t\n\f ]$/) || c == '/' || c == "v" || c == EOF) {
+		return afterAttributeName(c);
+	} else if(c == '=') {
+		return beforeAttributeValue;
+	} else if(c == '\u0000') {
+
+	} else if(c == '\"' || c == '\'' || c == '<') {
+
+	} else {
+		currentAttribute.name += c;
+		return attributeName;
+	}
+}
+
+function beforeAttributeValue(c) {
+	if(c.match(/^[\t\n\f ]$/) || c =='/' || c == '>' || c == EOF) {
+		return beforeAttributeValue;
+	} else if(c == '\"') {
+		return doubuleQuotedAttributeValue;
+	} else if(c== '\'') {
+		return singleQuotedAttributeValue;
+	} else if(c == '>') {
+		 
+	} else {
+		return UnquotedAttributeValue(c);
+	}
+}
+
+function doubuleQuotedAttributeValue(c){
+	if(c == "\"") {
+		currentToken[currentAttribute.name] = currentAttribute.value;
+		return afterQuotedAttributeValue;
+	} else if(c == '\u0000') {
+
+	} else if(c == EOF) {
+		 
+	} else {
+		currentAttribute.value += c;
+		return doubuleQuotedAttributeValue;
+	}
+}
+
+function singleQuotedAttributeValue(c){
+	if(c == '\'') {
+		currentToken[currentAttribute.name] = currentAttribute.value;
+		return afterQuotedAttributeValue;
+	} else if(c == '\u0000') {
+
+	} else if(c == EOF) {
+		 
+	} else {
+		currentAttribute.value += c;
+		return singleQuotedAttributeValue;
+	}
+}
+
+function afterQuotedAttributeValue(c){
+	if(c.match(/^[\t\n\f ]$/)) {
+		return beforeAttributeName;
+	} else if(c == '/') {
+		return selfClosingStartTag;
+	} else if(c == '>') {
+		currentToken[currentAttribute.name] = currentAttribute.value;
+		emit(currentToken);
+		return data;
+	} else if(c == EOF) {	
+
+	} else {
+		currentAttribute.value += c;
+		return afterQuotedAttributeValue;
+	}
+}
+
+function UnquotedAttributeValue(c){
+	if(c.match(/^[\t\n\f ]$/)) {
+		currentToken[currentAttribute.name] = currentAttribute.value;
+		return beforeAttributeName;
+	} else if(c == '/') {
+		currentToken[currentAttribute.name] = currentAttribute.value;
+		return selfClosingStartTag;
+	} else if(c == '>') {
+		currentToken[currentAttribute.name] = currentAttribute.value;
+		emit(currentToken);
+		return data; 
+	} else if(c == '\u0000') {
+
+	} else if(c == '\"' || c == "'" || c == "<" || c == '=' || c == '`') {
+
+	} else if(c == EOF) {
+
+	} else {
+		currentAttribute.value += c;
+		return UnquotedAttributeValue;
+	}
+
 }
 
 function selfClosingStartTag(c){
@@ -68,13 +194,13 @@ function selfClosingStartTag(c){
 
 	}
 }
-*/
+
 module.exports.parseHTML = function parseHTML(html){
- /* let state = data;
+  let state = data;
     for(let c of html) {
         state = state(c);
     }
-    state = state(EOF); */
-	console.log(html);
-	console.log(html)
+    state = state(EOF); 
+/*  console.log(html);
+	console.log(html)*/
 }
