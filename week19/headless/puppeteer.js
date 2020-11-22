a5e7a85a1207bc46a4afb152868dd85be00b0714
Brian <brian_liang95827@163.com>
2020-11-22 08:59:57 +0800

    headless

---
+++ week19/headless/puppeteer.js
@@ -0,0 +1,10 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  const hrefElement = await page.$('a');		//返回某个dom元素
+  await hrefElement.click();
+  // ...
+})();
